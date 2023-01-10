e479f21948d255052e9213e677d6bca18f86c666
JoseGerardo27 <121908039+JoseGerardo27@users.noreply.github.com>
2023-01-09 17:37:11 -0800

    a

--- database/migrations/2023_01_06_233157_create_colaboradors_table.php
+++ database/migrations/2023_01_06_233157_create_colaboradors_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('departamento');
             $table->string('doc_index')->nullable();
             $table->unsignedInteger('id_rol')->unsigned();
-            $table->string('folio');
+            $table->string('folio')->nullable();
             $table->foreign('id_rol')->references('id')->on('rols')->onDelete('cascade'); // Padre e hijo eliminados
         });
     }
